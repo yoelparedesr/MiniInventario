2e3b6a7884744e837780496b801ed1dd35651d2b
yoelparedesr <yoelparedesr@gmail.com>
2026-04-26 17:59:42 +0200

    subidos

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP Para <b>ejecutar</b> el código, pulsar <shortcut actionId="Run"/> o
+// Haz clic en el ícono <icon src="AllIcons.Actions.Execute"/> del margen.
+void main() {
+    //TIP Pulsa <shortcut actionId="ShowIntentionActions"/> con tu cursor en el texto resaltado
+    // para ver cómo IntelliJ IDEA sugiere corregirlo.
+    IO.println(String.format("Hello and welcome!"));
+
+    for (int i = 1; i <= 5; i++) {
+        //TIP Presione <shortcut actionId="Debug"/> para iniciar la depuración de su código. Hemos creado un punto de interrupción <icon src="AllIcons.Debugger.Db_set_breakpoint"/>
+        // para ti, pero siempre puedes añadir más pulsando <shortcut actionId="ToggleLineBreakpoint"/>.
+        IO.println("i = " + i);
+    }
+}
